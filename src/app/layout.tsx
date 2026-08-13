ceb3b8611aef1539842c8f0cc1dca14ee37f92df
k-mama <emmaestro123@gmail.com>
2026-08-13 15:53:16 +1000

    fix: force refreshed favicon asset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import "@/styles/global.css";
 
 const siteUrl = "https://esthercho.pages.dev";
 const socialPreviewImage = "/media/home/esther-house-entry-poster.jpg";
-const faviconUrl = "/favicon-esther-cho.svg?v=20260813-1534";
+const faviconUrl = "/favicon-esther-cho.svg?v=20260813-1547";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
