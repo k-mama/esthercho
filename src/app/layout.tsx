6249df101f83210c3aa8282545236f413bc49e8f
k-mama <emmaestro123@gmail.com>
2026-08-13 17:50:47 +1000

    Refresh favicon cache version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import "@/styles/global.css";
 
 const siteUrl = "https://esthercho.pages.dev";
 const socialPreviewImage = "/media/home/esther-house-entry-poster.jpg";
-const faviconUrl = "/favicon-esther-cho.svg?v=20260813-1547";
+const faviconUrl = "/favicon-esther-cho.svg?v=20260813-1743";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
