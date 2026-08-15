7dcb8f30f16b922e9a8bad064f996de52287bef7
k-mama <emmaestro123@gmail.com>
2026-08-15 12:08:30 +1000

    feat: publish sitemap discovery rules

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://esthercho.pages.dev/sitemap.xml",
+  };
+}
