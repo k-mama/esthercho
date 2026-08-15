9f89c4d240ae833da8eee5ddb91374939708d8aa
k-mama <emmaestro123@gmail.com>
2026-08-15 13:17:33 +1000

    style: load Korean typography layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { SiteFooter } from "@/components/site-footer";
 import { DocumentLanguage } from "@/components/document-language";
 import "@/styles/tokens.css";
 import "@/styles/global.css";
+import "@/styles/korean.css";
 import "@/styles/shell-redteam.css";
 
 const siteUrl = "https://esthercho.pages.dev";
