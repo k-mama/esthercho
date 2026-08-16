363e02bfd1c20df8ce29c501a418c3f0f2ea4507
k-mama <emmaestro123@gmail.com>
2026-08-16 14:20:52 +1000

    poc: add Keystatic admin landing route

---
+++ poc/keystatic-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Esther Cho Keystatic PoC</h1>
+      <p>This isolated app exists only to test the author-facing content workflow.</p>
+      <Link href="/keystatic">Open Keystatic</Link>
+    </main>
+  );
+}
