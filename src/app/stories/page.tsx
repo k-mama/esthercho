9bd4eb0c41b15b69089aa7c1de1810df3038012e
k-mama <emmaestro123@gmail.com>
2026-08-16 16:43:24 +1000

    style: apply stories polish

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import { PageCover } from "@/components/page-cover";
 import "./stories.css";
+import "./stories-polish.css";
 
 export const metadata: Metadata = {
   title: "Stories",
