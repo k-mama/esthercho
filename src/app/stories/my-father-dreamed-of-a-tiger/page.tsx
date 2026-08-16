ff52aaf7449ff681b3a271deb17085531817f3f2
k-mama <emmaestro123@gmail.com>
2026-08-16 13:10:44 +1000

    fix: restore first story archive image path

--- src/app/stories/my-father-dreamed-of-a-tiger/page.tsx
+++ src/app/stories/my-father-dreamed-of-a-tiger/page.tsx
@@ -57,7 +57,7 @@ export default function TigerDreamStoryPage() {
 
         <figure className="story-detail-figure">
           <Image
-            src="/media/home/childhood-garden.jpg"
+            src="/media/esther/childhood-garden.jpg"
             alt="Esther Cho smiling in a garden as a child"
             width={1800}
             height={1350}
